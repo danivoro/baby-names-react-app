1d739ea3dda6705b5b6f170c67ab72aaac6b42d3
dani voro <mr.voro@gmail.com>
2023-07-09 20:17:33 +0200

    names sorted

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,25 @@ function App(): JSX.Element {
       return 'boy'
     }
   }
+
+  interface MyObject {
+    id: number;
+    name: string;
+    sex: string;
+  }
+
+  function sortObjectsByName(arr: MyObject[]): MyObject[] {
+    const sortedArr = [...arr];
+    sortedArr.sort((a, b) => a.name.localeCompare(b.name));
+    return sortedArr;
+  }
+  
   
   return(
     <div>
       <p>Baby Names App</p>
       
-      {namesData.map((item, index) => (
+      {sortObjectsByName(namesData).map((item, index) => (
         <span className={classForNames(item.sex)} key={index}>{item.name} </span>
     )) 
     }
