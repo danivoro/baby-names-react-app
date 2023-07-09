684d62f2fc908d08129491aa3c54737e3017acad
dani voro <mr.voro@gmail.com>
2023-07-09 19:59:41 +0200

    first part finished: displaying names

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { greet } from "./utils/greet";
-import "./babyNamesData.json";
+import namesData from "./babyNamesData.json";
+import "./App.css"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+
+  const classForNames = (gender: string) => {
+    if (gender === 'f'){
+      return 'girl'
+    } else {
+      return 'boy'
+    }
+  }
+  
+  return(
+    <div>
+      <p>Baby Names App</p>
+      
+      {namesData.map((item, index) => (
+        <span className={classForNames(item.sex)} key={index}>{item.name} </span>
+    )) 
+    }
+   </div>
+  ) 
+  
 }
 
 export default App;
