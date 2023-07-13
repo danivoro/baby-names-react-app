cc5af5cfba5b0d87afa2dece10eeb5d77b4e6007
dani voro <mr.voro@gmail.com>
2023-07-13 13:49:50 +0200

    adding the search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import namesData from "./babyNamesData.json";
+import React, { useState } from "react";
 import "./App.css";
 
+interface KeyboardControlledInputProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function KeyboardControlledInput(props: KeyboardControlledInputProps): JSX.Element {
+  return (
+    <>
+      <input
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </>
+  );
+}
+
+
 function App(): JSX.Element {
+  const [typedMessage, setTypedMessage] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTypedMessage(event.target.value);
+  };
+  
   const classForNames = (gender: string) => {
     if (gender === "f") {
       return "girl";
@@ -22,14 +46,27 @@ function App(): JSX.Element {
     return sortedArr;
   }
 
+  function filterObjectByInput(arr: MyObject[]): MyObject[] {
+    const filteredArr = arr.filter((item) => item.name.toLowerCase().startsWith(typedMessage.toLowerCase()))
+    return filteredArr
+  }
+
   return (
     <div>
       <p>Baby Names App</p>
-
-      {sortObjectsByName(namesData).map((item, index) => (
+      <div className="searchBar">
+        <KeyboardControlledInput 
+          value={typedMessage}
+          onChange={handleInputChange}
+        />
+      </div>
+      
+      {filterObjectByInput(sortObjectsByName(namesData)).map((item, index) => (
+        
         <span className={classForNames(item.sex)} key={index}>
           {item.name}{" "}
         </span>
+
       ))}
     </div>
   );
